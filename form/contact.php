5cb84c0eaf4ec3d1a9d8b26e50415debc0ec79b5
root <root@kfriends-admin-lunamint.com>
2020-05-21 16:36:27 +0900

    email edited

--- form/contact.php
+++ form/contact.php
@@ -9,7 +9,7 @@ $emailTO = $emailBCC =  $emailCC = array(); $formEmail = '';
 $sitename = 'https://www.lunamint.com';
 
 ### Enter your email addresses: @required
-$emailTO[] = array( 'email' => 'atlas@lunamint.com', 'name' => 'Lunamint Atlas' ); 
+$emailTO[] = array( 'email' => 'admin@lunamint.com', 'name' => 'Lunamint Support' ); 
 
 ### Enable bellow parameters & update your BCC email if require.
 //$emailBCC[] = array( 'email' => 'email@yoursite.com', 'name' => 'Your Name' );
@@ -18,7 +18,7 @@ $emailTO[] = array( 'email' => 'atlas@lunamint.com', 'name' => 'Lunamint Atlas' 
 //$emailCC[] = array( 'email' => 'email@yoursite.com', 'name' => 'Your Name' );
 
 ### Enter Email Subject
-$subject = "Contact Us " . ' - ' . $sitename; 
+$subject = "사이트 문의[".$sitename."]"; 
 
 ### If your did not recive email after submit form please enable below line and must change to your correct domain name. eg. noreply@example.com
 $formEmail = 'noreply@lunamint.com';
@@ -26,6 +26,17 @@ $formEmail = 'noreply@lunamint.com';
 ### Success Messages
 $msg_success = "We have <strong>successfully</strong> received your message. We'll get back to you soon.";
 
+#test
+/*
+$_SERVER['REQUEST_METHOD'] = 'POST';
+$_POST["contact-email"] = 'benjamin@lunamint.com';
+$_POST["contact-name"] = 'test';
+$_POST["contact-message"] = 'test  msg';
+$_POST["form-anti-honeypot"] = '';
+$_SERVER['HTTP_REFERER'] = '';
+#php /var/www/html/lunamint/form/contact.php
+$debug  = 2;
+ */
 if( $_SERVER['REQUEST_METHOD'] == 'POST') {
 	if (isset($_POST["contact-email"]) && $_POST["contact-email"] != '' && isset($_POST["contact-name"]) && $_POST["contact-name"] != '') {
 		### Form Fields
@@ -34,7 +45,7 @@ if( $_SERVER['REQUEST_METHOD'] == 'POST') {
 		$cf_message = isset($_POST["contact-message"]) ? $_POST["contact-message"] : '';
 
 		$honeypot 	= isset($_POST["form-anti-honeypot"]) ? $_POST["form-anti-honeypot"] : 'bot';
-		$bodymsg = '';
+		$bodymsg = '사이트 문의내역 : <br>';
 		
 		if ($honeypot == '' && !(empty($emailTO))) {
 			### If you want use SMTP 
@@ -44,20 +55,20 @@ if( $_SERVER['REQUEST_METHOD'] == 'POST') {
 			// 0 = off (for production use)
 			// 1 = client messages
 			// 2 = client and server messages
-			$mail->SMTPDebug = 0;
+			$mail->SMTPDebug = isset($debug) ? $debug : 0;
 			//Set the hostname of the mail server
 			$mail->Host = 'smtp.gmail.com';
 			// use
 			// $mail->Host = gethostbyname('smtp.gmail.com');
 			// if your network does not support SMTP over IPv6
 			//Set the SMTP port number - 587 for authenticated TLS, a.k.a. RFC4409 SMTP submission
-			$mail->Port = 587;
+			$mail->Port = 587;	//587;	//465;
 			//Set the encryption system to use - ssl (deprecated) or tls
-			$mail->SMTPSecure = 'tls';
+			$mail->SMTPSecure = 'tls';	//'tls';	//'ssl';
 			//Whether to use SMTP authentication
 			$mail->SMTPAuth = true;
 			//Username to use for SMTP authentication - use full email address for gmail
-			$mail->Username = "lunamint.site@gmail.com";
+			$mail->Username = "lunamint.stieadmin@lunamint.com";
 			//Password to use for SMTP authentication
 			$mail->Password = "Mlnkbj12!@";
 
@@ -69,6 +80,14 @@ if( $_SERVER['REQUEST_METHOD'] == 'POST') {
 			$mail->FromName = $cf_name . ' - ' . $sitename;
 			$mail->AddReplyTo($cf_email, $cf_name);
 			$mail->Subject = $subject;
+			
+			/*$mail->SMTPOptions = array(
+      				"ssl" => array(
+				        "verify_peer" => false,
+             				"verify_peer_name" => false,
+	             			"allow_self_signed" => true
+        			)
+			);*/
 			
 			foreach( $emailTO as $to ) {
 				$mail->AddAddress( $to['email'] , $to['name'] );
